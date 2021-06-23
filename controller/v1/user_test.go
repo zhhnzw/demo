79b75c30929c9d37d0fe6c6a7e5153a96977a4f0
zhhnzw <747624368@qq.com>
2021-06-23 19:06:32 +0800

    update

--- controller/v1/user_test.go
+++ controller/v1/user_test.go
@@ -1,12 +1,32 @@
 package v1
 
 import (
+	"demo/dao/mysql"
+	"demo/settings"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+// 需要用数据库的单元测试需要对其初始化
+func init() {
+	cfg := settings.MySQLConfig{
+		Host:         "127.0.0.1",
+		User:         "root",
+		Password:     "root",
+		DbName:       "test",
+		Port:         3306,
+		MaxOpenConns: 200,
+		MaxIdleConns: 50,
+	}
+	if err := mysql.Init(&cfg); err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+}
 
 func TestGetUsers(t *testing.T) {
 	gin.SetMode(gin.TestMode)
